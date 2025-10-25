a2586903436a00aa66347fe1a8698842436548fc
Tuxium <ursusfeline07@gmail.com>
2025-10-25 04:12:24 +0000

    开始写创建实例函数

--- src/project_manager/create.rs
+++ src/project_manager/create.rs
@@ -1,173 +1,127 @@
+use std::error::Error;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use colored::Colorize;
-use uuid::Uuid;
 use crate::project_manager::Config;
-use crate::project_manager::config::{Api, ApiPermission, Backup, Event, Java, JavaType, PluginManage, Project, Runtime, ServerType, Time, Token};
+use crate::project_manager::config::{Backup, Event, Java, JavaType, PluginManage, Project, Runtime, ServerType, Time};
 use crate::project_manager::get_info::{get_info, NotValid};
+use crate::project_manager::tools::{VersionInfo, VersionType};
 
-struct Version {
-    major: u32,
-    minor: u32,
-    patch: u32,
-}
-
-impl Version {
-    fn from_str(s: &str) -> Result<Self, String> {
-        let parts: Vec<&str> = s.trim().split('.').collect();
-
-        if parts.len() != 3 {
-            return Err("The version number must be in x.x.x format".to_string());
-        }
-
-        let major = parts[0].parse()
-            .map_err(|_| format!("Invalid major version number:{}", parts[0]))?;
-        let minor = parts[1].parse()
-            .map_err(|_| format!("Invalid minor version number:{}", parts[1]))?;
-        let patch = parts[2].parse()
-            .map_err(|_| format!("Invalid patch version number:{}", parts[2]))?;
-
-        Ok(Version { major, minor, patch })
-    }
-    fn to_string(&self) -> String{
-        format!("{}.{}.{}",&self.major,&self.minor,&self.patch)
-    }
-}
-
+/// 获取一行输入，带有提示符 `>`
 fn get_input()->String{
-        let mut input_buffer = String::new();
-        print!(">");
-        std::io::Write::flush(&mut std::io::stdout()).expect("ERROR: Failed to print the prompt message");
-        match std::io::stdin().read_line(&mut input_buffer) {
-            Ok(_) => input_buffer,
-            Err(_)=> panic!("Unknown input error!")
-        }
+    // 初始化输入缓存
+    let mut input_buffer = String::new();
+    // 打印提示符
+    print!(">");
+    std::io::Write::flush(&mut std::io::stdout()).expect("ERROR: Failed to print the prompt message");
+    // 处理错误
+    match std::io::stdin().read_line(&mut input_buffer) {
+        Ok(_) => input_buffer,
+        Err(_)=> panic!("Unknown input error!")
+    }
 }
 
-fn get_config()->Config{
+fn check_dir(path: PathBuf)->bool{todo!()}
 
+/// 询问用户配置信息
+fn create_config()->Config{
+
+    // 创建基本配置
+    let mut new_config = Config::default();
+
+    // 获取实例名称
     println!("Enter the name of this project:");
-    let project_name = get_input().trim().to_string();
+    new_config.project.name = get_input().trim().to_string();
 
+    // 列出服务端类型
     println!("Select the type of the server:");
-    println!("1: Minecraft Server(Official)");
+    println!("1: Vanilla(Official)");
     println!("2: PaperMC");
     println!("3: PurpurMC");
     println!("4: SpigotMC");
-    println!("5: Minecraft Server for Bedrock Edition(Official)");
+    println!("5: Bedrock Dedicated Server(Official)");
     println!("0: Other Server");
-    let server_type = 'out: loop {
-        match get_input().trim().parse::<usize>() {
-            Ok(v)=>{
-                break loop {
-                    match v {
-                        1 => break ServerType::OfficialJE,
-                        2 => break ServerType::Paper,
-                        3 => break ServerType::Purpur,
-                        4 => break ServerType::Spigot,
-                        5 => break ServerType::OfficialBE,
-                        0 => break ServerType::Other,
-                        _ => {
-                            println!("Please select within the range.");
-                            continue 'out
-                        }
-                    }
-                }
-            },
-            Err(_)=>println!("Please enter a number.")
-        }
+    new_config.project.server_type = loop {
+        // 获取输入
+        let input = get_input().trim().parse::<usize>();
+        // 解析输入为 usize
+        let input = match input {
+            Ok(v)=>v,
+            Err(_)=>{
+                println!("Please enter a number.");
+                continue
+            }
+        };
+        // 解析输入为 ServerType
+        let input= match input {
+            1 => break ServerType::Vanilla,
+            2 => break ServerType::Paper,
+            3 => break ServerType::Purpur,
+            4 => break ServerType::Spigot,
+            5 => break ServerType::BDS,
+            0 => break ServerType::Other,
+            _ => {
+                println!("Please select within the range.");
+                continue
+            }
+        };
     };
 
-    let server_execute = match server_type {
+    // 根据服务端类型设置可执行文件
+    new_config.project.execute = match new_config.project.server_type {
+        // 自定义的可执行文件
         ServerType::Other=>{
             println!("Enter the name of the executable file");
             get_input()
         },
-        ServerType::OfficialBE=>"server".to_string(),
+        // BE 版可执行文件
+        ServerType::BDS=>"server".to_string(),
+        // JE 版可执行文件
         _ => "server.jar".to_string()
     };
 
-    println!("Set the game version. The default is the latest version. The format is x.x.x");
-    let server_version = loop {
-        match Version::from_str(&get_input()) {
-            Ok(v)=>break v,
-            Err(e)=> println!("{}",e)
+    // 获取并验证版本号
+    if new_config.project.server_type.eq(&ServerType::BDS){// 先检查基岩版
+        println!("Set the game version.");// 提示信息
+        let input = get_input().trim().to_string();//输入
+        new_config.project.version = loop {//确保输入完成
+            match VersionInfo::get_version_info(&input, ServerType::BDS) {
+                Ok(v) => {
+                    break v.name
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    println!("{}", "Please re-enter the version".yellow());
+                    continue;
+                }
+            }
+        };
+    }else if new_config.project.server_type.ne(&ServerType::Other){// 再检查Java版
+    println!("Set the game version. The default is the latest version.");// 提示信息
+    let input = get_input().trim().to_string();//输入
+    new_config.project.version = match &input[..]{
+        // 默认为最新Release版本
+        ""=>VersionInfo::get_latest_version(VersionType::Release).unwrap(),
+        "latest"=>VersionInfo::get_latest_version(VersionType::Release).unwrap(),
+        _ => {
+            // 手动设置版本
+            loop {//确保输入完成
+                match VersionInfo::get_version_info(&input, new_config.project.server_type.clone()) {
+                    Ok(v) => {
+                        new_config.project.version_type=v.version_type;
+                        break v.name
+                    }
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        println!("{}", "Please re-enter the version".yellow());
+                        continue;
+                    }
+                }
+            }
         }
-    };
+    };}
 
-    let server_birthday = chrono::Utc::now().to_rfc3339();
-
-    let project = Project{
-        name:project_name,
-        server_type,
-        execute:server_execute,
-        version:server_version.to_string(),
-        birthday:server_birthday
-    };
-
-    let runtime_java = Java{
-        version:21,
-        edition:JavaType::GraalVM,
-        arguments:vec![],
-        custom:String::new(),
-        xms:0,
-        xmx:0
-    };
-
-    let runtime = Runtime{
-        java:runtime_java
-    };
-
-    let backup_time=Time{
-        interval:0,
-        cron:String::new()
-    };
-
-    let backup_event=Event{
-        start:false,
-        stop:true,
-        update:true
-    };
-
-    let backup = Backup{
-        enable : true,
-        world:true,
-        other:true,
-        time: Option::from(backup_time),
-        event: Option::from(backup_event)
-    };
-
-    let plugin_manage = PluginManage{
-        manage : true,
-    };
-
-    let manage_token=Token{
-        value:Uuid::new_v4().to_string(),
-        permission:ApiPermission::Manage
-    };
-
-    let status_token=Token{
-        value:Uuid::new_v4().to_string(),
-        permission:ApiPermission::Status
-    };
-
-    let api = Api{
-        enable:false,
-        listen:".nmsl/socket/api.socket".to_string(),
-        token:vec![
-            manage_token,
-            status_token
-        ]
-    };
-
-    Config{
-        project,
-        runtime,
-        backup,
-        plugin_manage,
-        api
-
-    }
+    new_config
 }
 
 pub fn create_project(){
@@ -177,7 +131,7 @@ pub fn create_project(){
             match e {
                 NotValid::ConfigBroken=>eprintln!("{}","There are files related to NMSL in the current directory, but they may be damaged. Please check the .nmsl directory and the NMSL.toml file. You need to manually delete them to continue creating.".yellow()),
                 NotValid::NotConfigured=>{
-                    let config = get_config();
+                    let config =create_config();
                     match config.to_file(Path::new("NMSL.toml")){
                         Ok(_)=>(),
                         Err(_)=>panic!("The configuration file cannot be created!")
